fceb13bc081872133b0b0f62b0bf8dc0592db394
MustafaKayra <m.toker2009@gmail.com>
2024-06-27 00:38:51 +0300

    database güncellendi

---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.CharField(max_length=30,null=False,blank=False)
+    title = models.CharField(max_length=100,null=False,blank=False)
+    content = models.TextField(null=False,blank=False)
+    email = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
