c97bcaba8c271cda508369dde944eac1a0055f7a
MustafaKayra <154830883+MustafaKayra@users.noreply.github.com>
2024-06-27 00:44:31 +0300

    Delete tweetproject directory

--- tweetproject/tweet/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('addtweet/',views.addtweet,name="addtweet")
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
