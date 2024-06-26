03f189ad2c0a73c800ccb651973500ebfdf8424c
MustafaKayra <154830883+MustafaKayra@users.noreply.github.com>
2024-06-27 00:44:24 +0300

    Delete tweet directory

--- tweet/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Tweet(models.Model):
-    name = models.CharField(max_length=50,null=False,blank=False)
-    email = models.CharField(max_length=50,null=False,blank=False)
-    content = models.TextField(null=False,blank=False)
-
-    def __str__(self):
-        return f"{self.content}"
