8e04a7b9d7aed35a15b5e8ee3bec705ebb7ac6d3
MustafaKayra <m.toker2009@gmail.com>
2024-07-10 18:24:17 +0300

    addpost sayfasına form eklendi

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.all()
@@ -8,5 +9,23 @@ def index(request):
     }
     return render(request,"index.html",context)
 
+def about(request):
+    return render(request,"about.html")
+
 def addpost(request):
-    return render(request,"addpost.html")
+    if request.method == "POST":
+        form = PostForm(request.POST or None)
+        if form.is_valid():
+            author = form.cleaned_data.get("author")
+            email = form.cleaned_data.get("email")
+            title = form.cleaned_data.get("title")
+            content = form.cleaned_data.get("content")
+
+            newPost = Post.objects.create(author=author,email=email,title=title,content=content)
+            newPost.save()
+
+            print('Post Başarıyla Kaydedildi(in terminal)')
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request,"addpost.html",{"form":form})
